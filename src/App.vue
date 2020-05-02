875e5b26fdb7f2a1dce29e9be092e2d3c522cacd
denizhanaydinli <daydinli93@gmail.com>
2020-05-02 12:29:26 +0300

    commit vue2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="container">
+    <div class="row">
+    <div class="col-md-8 offset-md-2 text-center">
+    <h3 class="mt-5">Todo List | Vue Js</h3>
+    <hr>
+ <div class="row">
+    <div class="col-md-6 offset-md-3 d-flex flex-row justify-content-between align-items-center">
+    <input type="text" v-model="todoText">
+
+    <button @click="addTodo" class="btn btn-primary">Ekle</button>
+    
+    
+    </div>
+    </div>
+    <hr>
+    <div class="todo-container">
+    <Todo @deleteTodo="deleteTodo($event)" v-for="todo in todoList" :todo="todo" :key="todo.id" />
+    </div>
+   
+    </div>
+ </div>
+ 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios"
+import Todo from "@/components/Todo"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components:{
+    Todo
+  },
+  data(){
+    return{
+      todoText : "",
+      todoList : []
+    }
+  },
+  methods:{
+    addTodo(){
+      axios.post("https://vuejs-1-video-30784.firebaseio.com/todoList.json",{text : this.todoText})
+      .then(response =>
+      console.log(response))
+      .catch(e => {
+        console.log(e)
+      })
+    },
+    deleteTodo(todoId){
+      axios.delete("https://vuejs-1-video-30784.firebaseio.com/todoList/" +todoId +".json")
+      .then(response => {
+        let index = this.todoList.findIndex(i =>{
+          return i.id == todoId
+        })
+
+      this.todoList.splice(index,1)
+
+      })
+      .catch(e =>{
+        console.log(e)
+      })
+    }
+   
+  },
+   created(){
+      axios.get("https://vuejs-1-video-30784.firebaseio.com/todoList.json")
+      .then(response =>{
+        this.todoList.push({
+          id : response.data.name,
+          text : this.todoText
+        })
+        for(let key in response.data){
+          //console.log(response.data[key])
+          let todo = {
+            text : response.data[key],
+            id : key
+          }
+          this.todoList.push(todo)
+        }
+      })
+      .catch(e => {
+        console.log(e)
+      })
+    }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
